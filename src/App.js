4214b3c01712bf611cb848c4b081530bc6435e57
Michael Emmanuel <michael.emmanuel732@gmail.com>
2017-09-06 19:50:48 -0400

    install boilerplate, modify app.js

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import TaskTracker from './TaskTracker';
+
+let cardsList = [
+  {
+    id: 1,
+    title: "Learn new language",
+    description: "I should learn how to speak French",
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Plan your next trip",
+    description: "Travel to a foreign island",
+    status: "todo",
+    tasks: [
+      {
+        id: 1,
+        name: "Costa Rica",
+        done: true
+      },
+      {
+        id: 2,
+        name: "Cancun",
+        done: false
+      },
+      {
+        id: 3,
+        name: "Dominican Republic",
+        done: false
+      }
+    ]
+  }
+];
+
+React.render(<TastTracker cards={cardsList} />, document.getElementById('root'));
