583489583cb6d7108f0e832a8395980bcc8fe2fd
Michael Emmanuel <michael.emmanuel732@gmail.com>
2017-09-06 20:27:33 -0400

    added final CheckList component

---
+++ src/CheckList.js
@@ -0,0 +1,22 @@
+// CheckList component will serve as the bottom part of the card
+
+import React, { Component } from 'react';
+
+class Checklist extends Component {
+  render() {
+    let tasks = this.props.tasks.map((task) => (
+      <li className="checklist_task">
+        <input type="checkbox" defaultChecked={task.done} />
+        {task.name}
+        <a href="#" className="checklist_task--remove" />
+      </li>
+    ));
+    return (
+      <div className="checklist">
+        <ul>{tasks}</ul>
+      </div>
+    );
+  }
+}
+
+export default CheckList;
